19ee55ee8142acd07ea9f507a58c50c83b016f60
Rebecca Girndt <r.l.girndt@gmail.com>
2023-02-02 18:13:54 -0600

    finished first three table functions

--- db/queries.sql
+++ db/queries.sql
@@ -2,9 +2,7 @@
 SELECT * FROM departments;
 
 -- presented with the job title, role id, the department that role belongs to, and the salary for that role
-SELECT roles.title AS title, roles.id AS id, departments.name AS department, roles.salary AS salary
-FROM roles
-JOIN departments ON roles.department_id = departments.id;
+SELECT roles.id AS id, roles.title AS title, departments.name AS department, roles.salary AS salary FROM roles JOIN departments ON roles.department_id = departments.id;
 
 -- a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name,' ', manager.last_name) AS manager
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
+    }, 
+    console.log('Connected to the company_db database.')
+);
 
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
     // function basePrompt(){ inquirer.prompt.then((response) => switch-case => otherFunction()) }
-function basePrompt() {
+function mainPrompt() {
     inquirer.prompt([
         {
             type: 'list',
@@ -42,23 +53,39 @@ function basePrompt() {
 
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
-    //function viewDepartments(){ db.query(select department table){ console.table(response) } .then(() => basePrompt()) }
+    //function viewDepartments(){ db.query(select department table){ console.table(response) } .then(() => mainPrompt()) }
 function viewDepartments() {
-    console.log('View all departments');
+
+    db.query('SELECT * FROM departments', function (err, results) {
+        console.table('', results);
+        mainPrompt();
+    });
 };
 
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-    //function viewRoles(){ db.query(select role table){ console.table(response) } .then(() => basePrompt()) }
+    //function viewRoles(){ db.query(select role table){ console.table(response) } .then(() => mainPrompt()) }
 function viewRoles() {
-    console.log('View all roles');
+
+    let sql = 'SELECT roles.id AS id, roles.title AS title, departments.name AS department, roles.salary AS salary FROM roles JOIN departments ON roles.department_id = departments.id'
+
+    db.query(sql, function (err, results) {
+        console.table('', results);
+        mainPrompt();
+    });
 };
 
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    //function viewEmployees(){ db.query(select employee table){ console.table(response) } .then(() => basePrompt()) }
+    //function viewEmployees(){ db.query(select employee table){ console.table(response) } .then(() => mainPrompt()) }
 function viewEmployees() {
-    console.log('View all employees');
+
+    let sql = "SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employees employee LEFT OUTER JOIN employees manager ON employee.manager_id = manager.id JOIN roles ON employee.role_id = roles.id JOIN departments ON roles.department_id = departments.id"
+
+    db.query(sql, function (err, results) {
+        console.table('', results);
+        mainPrompt();
+    })
 };
 
 // WHEN I choose to add a department
@@ -95,4 +122,4 @@ function updateEmployeeRole() {
         
 //look into inquirer docs for choices, can have own msg and name keys?
 
-basePrompt();
+mainPrompt();
